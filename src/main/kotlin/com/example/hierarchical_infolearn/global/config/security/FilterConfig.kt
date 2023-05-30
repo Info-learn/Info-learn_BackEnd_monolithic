4d28a1a91ed4e9f744d0366c6aee7806910842c2
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 11:29:01 +0900

    🚚 :: path 변경

--- src/main/kotlin/com/example/hierarchical_infolearn/global/config/security/FilterConfig.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/global/config/security/FilterConfig.kt
@@ -1,17 +1,17 @@
-package com.example.hierarchical_infolearn.global.security.config
+package com.example.hierarchical_infolearn.global.config.security
 
 import com.example.hierarchical_infolearn.global.error.filter.ExceptionFilter
-import com.example.hierarchical_infolearn.global.security.jwt.JwtFilter
-import com.example.hierarchical_infolearn.global.security.jwt.TokenProvider
-import com.example.hierarchical_infolearn.global.security.jwt.auth.StudentDetailsService
-import com.example.hierarchical_infolearn.global.security.jwt.auth.TeacherDetailsService
+import com.example.hierarchical_infolearn.global.config.security.jwt.JwtFilter
+import com.example.hierarchical_infolearn.global.config.security.jwt.TokenProvider
+import com.example.hierarchical_infolearn.global.config.security.jwt.auth.StudentDetailsService
+import com.example.hierarchical_infolearn.global.config.security.jwt.auth.TeacherDetailsService
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-class FilterConfiguration(
+class FilterConfig(
     private val tokenProvider: TokenProvider,
     private val studentDetails: StudentDetailsService,
     private val teacherDetails: TeacherDetailsService,
