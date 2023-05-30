f7924e395599d821c88857d96567ce1556ffbba9
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 10:26:16 +0900

    📝 :: Exceptions Class -> Object

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/lecture/exception/TooManyLectureTag.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/lecture/exception/TooManyLectureTag.kt
@@ -1,7 +1,6 @@
 package com.example.hierarchical_infolearn.domain.lecture.exception
 
 import com.example.hierarchical_infolearn.global.error.ErrorCode
-import com.example.hierarchical_infolearn.global.error.data.GlobalError
+import com.example.hierarchical_infolearn.global.error.data.GlobalException
 
-class TooManyLectureTag(data: String): GlobalError(ErrorCode.TOO_MANY_LECTURE_TAG, data) {
-}
+object TooManyLectureTag: GlobalException(ErrorCode.TOO_MANY_LECTURE_TAG)
