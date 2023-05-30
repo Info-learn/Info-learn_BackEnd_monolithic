732ba2454e8de78c2a2ae78a3a610ca631cc32a9
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 10:28:32 +0900

    📝 :: Exceptions Class -> Object

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/user/exception/UserNotFoundException.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/user/exception/UserNotFoundException.kt
@@ -1,6 +1,6 @@
 package com.example.hierarchical_infolearn.domain.user.exception
 
 import com.example.hierarchical_infolearn.global.error.ErrorCode
-import com.example.hierarchical_infolearn.global.error.data.GlobalError
+import com.example.hierarchical_infolearn.global.error.data.GlobalException
 
-class UserNotFoundException(data: String): GlobalError(ErrorCode.USER_NOT_FOUND, data)
+object UserNotFoundException: GlobalException(ErrorCode.USER_NOT_FOUND)
