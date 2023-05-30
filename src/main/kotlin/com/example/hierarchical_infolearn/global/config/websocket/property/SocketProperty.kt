c26cc4d65947e6db89f5173635dc0cd91d304467
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 10:48:24 +0900

    📝 :: socket property

---
+++ src/main/kotlin/com/example/hierarchical_infolearn/global/config/websocket/property/SocketProperty.kt
@@ -0,0 +1,10 @@
+package com.example.hierarchical_infolearn.global.config.websocket.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "socket")
+data class SocketProperty(
+    val port: Int
+)
