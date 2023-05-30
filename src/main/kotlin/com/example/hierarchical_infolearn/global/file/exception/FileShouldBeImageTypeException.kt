c5e821b0d64258898d00d173dae7f27128647f45
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 10:40:48 +0900

    📝 :: Exceptions Class -> Object

--- src/main/kotlin/com/example/hierarchical_infolearn/global/file/exception/FileShouldBeImageTypeException.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/global/file/exception/FileShouldBeImageTypeException.kt
@@ -1,8 +1,6 @@
 package com.example.hierarchical_infolearn.global.file.exception
 
 import com.example.hierarchical_infolearn.global.error.ErrorCode
-import com.example.hierarchical_infolearn.global.error.data.GlobalError
-import com.example.hierarchical_infolearn.global.file.type.ImageExt
+import com.example.hierarchical_infolearn.global.error.data.GlobalException
 
-class FileShouldBeImageTypeException(): GlobalError(ErrorCode.FILE_SHOULD_BE_IMAGE_TYPE, ImageExt.values().map {it.extension}.toString()) {
-}
+object FileShouldBeImageTypeException: GlobalException(ErrorCode.FILE_SHOULD_BE_IMAGE_TYPE)
