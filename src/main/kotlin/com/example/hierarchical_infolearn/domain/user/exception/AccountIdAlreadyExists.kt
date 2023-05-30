b5a8fcd3cf8e252eb89572cffc88aecaccc774b3
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 10:27:43 +0900

    📝 :: Exceptions Class -> Object

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/user/exception/AccountIdAlreadyExists.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/user/exception/AccountIdAlreadyExists.kt
@@ -1,7 +1,7 @@
 package com.example.hierarchical_infolearn.domain.user.exception
 
 import com.example.hierarchical_infolearn.global.error.ErrorCode
-import com.example.hierarchical_infolearn.global.error.data.GlobalError
+import com.example.hierarchical_infolearn.global.error.data.GlobalException
 
 
-class AccountIdAlreadyExists(data: String): GlobalError(ErrorCode.ACCOUNT_ID_ALREADY_EXISTS, data)
+object AccountIdAlreadyExists: GlobalException(ErrorCode.ACCOUNT_ID_ALREADY_EXISTS)
