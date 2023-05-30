fc733c2acdfa1cbaa985951c34d8fde84a7b4c7f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-30 10:48:06 +0900

    📝 :: sockJS -> netty socket.io

---
+++ src/main/kotlin/com/example/hierarchical_infolearn/global/config/websocket/WebSocketConfig.kt
@@ -0,0 +1,37 @@
+package com.example.hierarchical_infolearn.global.config.websocket
+
+import com.corundumstudio.socketio.SocketIOServer
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner
+import com.example.websocketdemo.global.config.websocket.listener.SocketExceptionListener
+import com.example.hierarchical_infolearn.global.config.websocket.property.SocketProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import com.corundumstudio.socketio.SocketConfig
+
+@Configuration
+class WebSocketConfig(
+    private val property: SocketProperty
+) {
+
+    @Bean
+    fun socketIOServer(): SocketIOServer {
+
+        val socketConfig = SocketConfig()
+        socketConfig.isReuseAddress = true
+
+        val configuration = com.corundumstudio.socketio.Configuration()
+
+        configuration.let {
+            it.port = property.port
+            it.origin = "*";
+            it.socketConfig = socketConfig;
+            it.exceptionListener = SocketExceptionListener()
+        }
+
+        return SocketIOServer(configuration);
+    }
+
+    @Bean
+    fun springAnnotationScanner(socketIOServer: SocketIOServer) = SpringAnnotationScanner(socketIOServer)
+
+}
