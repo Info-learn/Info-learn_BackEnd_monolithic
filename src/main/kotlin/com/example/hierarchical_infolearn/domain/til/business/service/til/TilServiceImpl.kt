c7455fa5e852a9d61a6c75807ca6b679120c85c0
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-06-13 19:30:26 +0900

    📝 :: tilUser 추가

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/til/business/service/til/TilServiceImpl.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/til/business/service/til/TilServiceImpl.kt
@@ -5,12 +5,10 @@ import com.example.hierarchical_infolearn.domain.til.business.dto.response.Creat
 import com.example.hierarchical_infolearn.domain.til.business.dto.response.TilContentImageResponse
 import com.example.hierarchical_infolearn.domain.til.data.entity.Til
 import com.example.hierarchical_infolearn.domain.til.data.entity.content.Content
+import com.example.hierarchical_infolearn.domain.til.data.entity.socket.TilUser
 import com.example.hierarchical_infolearn.domain.til.data.entity.tag.Tag
 import com.example.hierarchical_infolearn.domain.til.data.entity.tag.TagUsage
-import com.example.hierarchical_infolearn.domain.til.data.repo.TilContentRepository
-import com.example.hierarchical_infolearn.domain.til.data.repo.TilRepository
-import com.example.hierarchical_infolearn.domain.til.data.repo.TilTagRepository
-import com.example.hierarchical_infolearn.domain.til.data.repo.TilTagUsageRepository
+import com.example.hierarchical_infolearn.domain.til.data.repo.*
 import com.example.hierarchical_infolearn.global.file.dto.ImageFileRequest
 import com.example.hierarchical_infolearn.global.file.dto.PreSignedUrlResponse
 import com.example.hierarchical_infolearn.global.utils.CurrentUtil
@@ -28,6 +26,7 @@ class TilServiceImpl(
     private val s3Util: S3Util,
     private val tilTagRepository: TilTagRepository,
     private val tilTagUsageRepository: TilTagUsageRepository,
+    private val tilTilUserRepository: TilUserRepository
 ): TilService {
 
     companion object {
@@ -39,14 +38,20 @@ class TilServiceImpl(
 
         val tilContentId = ObjectId.get()
 
+        println(tilContentId)
+        println(tilContentId.toString())
+        println(tilContentId.toString().length)
+
+        val user = currentUtil.getCurrentUser()
+
         val til = tilRepository.save(
             Til(
                 title = req.title,
                 searchTitle = req.searchTitle,
                 subTitle = req.subTitle,
                 isPrivate = req.isPrivate,
-                tilContent = tilContentId,
-                user = currentUtil.getCurrentUser()
+                tilContent = tilContentId.toString(),
+                user = user
             )
         )
 
@@ -81,8 +86,12 @@ class TilServiceImpl(
             return CreateTilResponse(til.id!!, preSignedUrl)
         }
 
-        return CreateTilResponse(til.id!!)
+        val tilUser = tilTilUserRepository.save(TilUser(user = user, til = til))
 
+        til.addTilUser(tilUser)
+        user.addTilUser(til, tilUser)
+
+        return CreateTilResponse(til.id!!)
     }
 
     override fun createImage(req: ImageFileRequest): TilContentImageResponse {
