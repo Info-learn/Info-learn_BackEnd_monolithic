f211adbeff8128a81835726275da3e68af51f32d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-06-13 19:09:14 +0900

    ♻️ :: IdClass를 내장 DataClass로 변경

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/lecture/data/repo/tag/LectureTagUsageRepository.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/lecture/data/repo/tag/LectureTagUsageRepository.kt
@@ -3,10 +3,9 @@ package com.example.hierarchical_infolearn.domain.lecture.data.repo.tag
 import com.example.hierarchical_infolearn.domain.lecture.data.entity.Lecture
 import com.example.hierarchical_infolearn.domain.lecture.data.entity.tag.Tag
 import com.example.hierarchical_infolearn.domain.lecture.data.entity.tag.TagUsage
-import com.example.hierarchical_infolearn.domain.lecture.data.entity.tag.TagUsageIdClass
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LectureTagUsageRepository: JpaRepository<TagUsage, TagUsageIdClass> {
+interface LectureTagUsageRepository: JpaRepository<TagUsage, TagUsage.IdClass> {
 
     fun existsByLectureAndTag(lecture: Lecture, tag: Tag): Boolean
     fun deleteByLectureAndTag(lecture: Lecture, tag: Tag)
