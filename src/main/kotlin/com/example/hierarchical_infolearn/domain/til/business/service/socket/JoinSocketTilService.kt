cd50b8777377e51666209f0e8339d1083b6bfe1e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-06-13 19:10:01 +0900

    🚚 :: Path변경

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/til/business/service/socket/JoinSocketTilService.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/til/business/service/socket/JoinSocketTilService.kt
@@ -1,4 +1,4 @@
-package com.example.hierarchical_infolearn.domain.til.business.service.til
+package com.example.hierarchical_infolearn.domain.til.business.service.socket
 
 import com.corundumstudio.socketio.SocketIOClient
 import com.example.hierarchical_infolearn.domain.til.business.dto.request.JoinSocketTIlRequest
