0482c19066a72ca4a5608df8320203086db16b6e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-06-13 09:39:32 +0900

    🔥 :: IdClass

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/lecture/data/entity/tag/TagUsageIdClass.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hierarchical_infolearn.domain.lecture.data.entity.tag
-
-import java.io.Serializable
-
-data class TagUsageIdClass(
-    val tag: String? = null,
-    val lecture: String? = null,
-): Serializable
