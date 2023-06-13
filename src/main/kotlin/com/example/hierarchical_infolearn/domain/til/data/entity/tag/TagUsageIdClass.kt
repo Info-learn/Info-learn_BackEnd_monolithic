37b7de89aff658aaf3b895bbf0565842f5cd6cab
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-06-13 19:48:00 +0900

    ♻️ :: IdClass를 내장 DataClass로 변경

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/til/data/entity/tag/TagUsageIdClass.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hierarchical_infolearn.domain.til.data.entity.tag
-
-import java.io.Serializable
-import java.util.UUID
-
-data class TagUsageIdClass (
-    val tag: String? = null,
-    val til: UUID? = null,
-): Serializable
