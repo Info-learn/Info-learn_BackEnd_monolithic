6df34ec2865ebb80f05bc8fc696482f8b54a6a37
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-06-13 19:47:46 +0900

    ♻️ :: IdClass를 내장 DataClass로 변경

--- src/main/kotlin/com/example/hierarchical_infolearn/domain/til/data/entity/tag/TagUsage.kt
+++ src/main/kotlin/com/example/hierarchical_infolearn/domain/til/data/entity/tag/TagUsage.kt
@@ -3,15 +3,17 @@ package com.example.hierarchical_infolearn.domain.til.data.entity.tag
 import com.example.hierarchical_infolearn.domain.til.data.entity.Til
 import com.example.hierarchical_infolearn.global.base.entity.BaseTimeEntity
 import org.springframework.data.domain.Persistable
+import java.io.Serializable
+import java.util.*
 import javax.persistence.*
 
-@Entity
-@Table(name = "tbl_til_tag_usage",uniqueConstraints = [UniqueConstraint(columnNames = ["tag_id","til_id"])])
-@IdClass(TagUsageIdClass::class)
+@Entity(name = "til_tag_usage")
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["tag_id","til_id"])])
+@IdClass(TagUsage.IdClass::class)
 class TagUsage(
     tag: Tag,
     til: Til
-): BaseTimeEntity(), Persistable<String> {
+): BaseTimeEntity(), Persistable<TagUsage.IdClass> {
 
     @Id @ManyToOne
     @JoinColumn(name = "tag_id", nullable = false, columnDefinition = "varchar(20) binary")
@@ -21,11 +23,12 @@ class TagUsage(
     @JoinColumn(name = "til_id", nullable = false, columnDefinition = "BINARY(16)")
     val til: Til = til
 
-    override fun getId(): String? {
-        return this.id
-    }
+    data class IdClass (
+        val tag: String? = null,
+        val til: UUID? = null,
+    ): Serializable
 
-    override fun isNew(): Boolean {
-        return this.createdAt == null
-    }
+    override fun getId() = IdClass(this.tag.id, this.til.id)
+
+    override fun isNew() = this.createdAt == null
 }
